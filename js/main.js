e09ccfab25c962b26d50f08c6bc5fb3e47fb9065
Crispin <crispinliu168@gmail.com>
2023-11-18 12:52:58 -0800

    Add main template

---
+++ js/main.js
@@ -0,0 +1,7 @@
+/**
+ * Load data from CSV file asynchronously and render charts
+ */
+// Use this when data is ready
+d3.csv("data/filtered_spotify.csv").then((data) => {
+  // BUILD CHARTS HERE
+});
