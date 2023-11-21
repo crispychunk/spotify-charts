f1b0ed57cdbe3b8d37530344be322aa1a766e5db
UBC Student <szhou13@students.cs.ubc.ca>
2023-11-21 15:31:35 -0800

    add prettier configuration (#6)

--- js/main.js
+++ js/main.js
@@ -1,8 +1,12 @@
 d3.csv('data/data.csv')
-.then ( data => {
-    const defaultCountry = 'Canada';
-    const defaultDate = '2022-06-16';
+    .then(data => {
+        const defaultCountry = 'Canada';
+        const defaultDate = '2022-06-16';
 
 
-    const slopeChart = new SlopeChart({parentElement: '#slope-chart', defaultCountry: defaultCountry, defaultDate: defaultDate}, data);
-})
+        const slopeChart = new SlopeChart({
+            parentElement: '#slope-chart',
+            defaultCountry: defaultCountry,
+            defaultDate: defaultDate
+        }, data);
+    })
